fbaecc6b82880c6d7319c474b44882619b25571b
Thomas Salm <thomas@devtom.de>
2023-06-04 16:59:32 +0200

    environment variable overwrites the predefined default value #18

--- mqtt_gateway.py
+++ mqtt_gateway.py
@@ -591,9 +591,8 @@ class MessageHandler:
 
 class EnvDefault(argparse.Action):
     def __init__(self, envvar, required=True, default=None, **kwargs):
-        if not default and envvar:
-            if envvar in os.environ:
-                default = os.environ[envvar]
+        if envvar in os.environ:
+            default = os.environ[envvar]
         if required and default:
             required = False
         super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
