9e2832a891d8e4cd25c4a7adb2643825ae2451d2
Giovanni Condello <giovanni.condello@coderit.it>
2024-04-05 22:01:09 +0200

    During charging, do not easily fall back on the active refresh period

--- vehicle.py
+++ vehicle.py
@@ -720,12 +720,14 @@ class VehicleState:
                 charge_status.chargingGunState
                 and is_valid_power
                 and charge_mgmt_data.decoded_power < -1
-                and remaining_charging_time > 0
         ):
             time_for_1pct = 36.0 * self.get_actual_battery_capacity() / abs(charge_mgmt_data.decoded_power)
             time_for_min_pct = math.ceil(self.charge_polling_min_percent * time_for_1pct)
             # It doesn't make sense to refresh less often than the estimated time for completion
-            computed_refresh_period = min(remaining_charging_time, time_for_min_pct)
+            if remaining_charging_time > 0:
+                computed_refresh_period = min(remaining_charging_time, time_for_min_pct)
+            else:
+                computed_refresh_period = time_for_1pct
             self.set_refresh_period_charging(computed_refresh_period)
         else:
             self.set_refresh_period_charging(0)
