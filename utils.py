565a174862f80b2704e81b1dc8af9dc117b9f986
Giovanni Condello <condellog@gmail.com>
2024-04-06 18:57:39 +0200

    Filter out invalid temp readings

--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
 def value_in_range(value, min_incl, max_excl) -> bool:
     return value is not None and min_incl <= value < max_excl
+
+
+def is_valid_temperature(value) -> bool:
+    return value_in_range(value, -127, 127) and value != 87
