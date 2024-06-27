fad8db17b427c0cee3f4a36c27ff0d8f2e961c03
Giovanni Condello <condellog@gmail.com>
2024-06-27 23:33:22 +0200

    Do not rely on unread message satus, keep track of the last message we processed instead

--- handlers/message.py
+++ handlers/message.py
@@ -1,4 +1,6 @@
+import datetime
 import logging
+from typing import Union
 
 from saic_ismart_client_ng import SaicApi
 from saic_ismart_client_ng.api.message.schema import MessageEntity
@@ -14,6 +16,8 @@ class MessageHandler:
     def __init__(self, gateway: VehicleHandlerLocator, saicapi: SaicApi):
         self.gateway = gateway
         self.saicapi = saicapi
+        self.last_message_ts = datetime.datetime.min
+        self.last_message_id = None
 
     async def check_for_new_messages(self) -> None:
         if self.__should_poll():
@@ -27,41 +31,39 @@ class MessageHandler:
 
     async def __polling(self):
         try:
-            unread_count = await self.saicapi.get_unread_messages_count()
-            LOG.info(f'{unread_count} unread messages')
-            if unread_count.alarmNumber == 0:
-                return
-            all_messages = await self.__get_all_messages()
+            all_messages = await self.__get_all_alarm_messages()
             LOG.info(f'{len(all_messages)} messages received')
 
-            vehicle_start_messages = [m for m in all_messages if m.messageType == '323']
-            latest_vehicle_start_message = self.__get_latest_message(vehicle_start_messages)
-
             new_messages = [m for m in all_messages if m.read_status != 'read']
-            latest_message = self.__get_latest_message(new_messages)
-
             for message in new_messages:
                 LOG.info(message.details)
                 await self.__read_message(message)
 
-            if latest_vehicle_start_message is not None:
+            latest_message = self.__get_latest_message(all_messages)
+            if latest_message.messageId != self.last_message_id and latest_message.message_time > self.last_message_ts:
+                self.last_message_id = latest_message.messageId
+                self.last_message_ts = latest_message.message_time
                 LOG.info(
-                    f'{latest_vehicle_start_message.title} detected at {latest_vehicle_start_message.message_time}')
-                vehicle_handler = self.gateway.get_vehicle_handler(latest_vehicle_start_message.vin)
-                if vehicle_handler:
-                    # delete the vehicle start message after processing it
-                    vehicle_handler.vehicle_state.notify_message(latest_vehicle_start_message)
-                await self.__delete_message(latest_vehicle_start_message)
-            elif latest_message is not None:
+                    f'{latest_message.title} detected at {latest_message.message_time}'
+                )
                 vehicle_handler = self.gateway.get_vehicle_handler(latest_message.vin)
                 if vehicle_handler:
                     vehicle_handler.vehicle_state.notify_message(latest_message)
+
+            # Delete vehicle start messages unless they are the latest
+            vehicle_start_messages = [
+                m for m in all_messages
+                if m.messageType == '323' and m.messageId != self.last_message_id
+            ]
+            for vehicle_start_message in vehicle_start_messages:
+                await self.__delete_message(vehicle_start_message)
+
         except SaicApiException as e:
             LOG.exception('MessageHandler poll loop failed during SAIC API Call', exc_info=e)
         except Exception as e:
             LOG.exception('MessageHandler poll loop failed unexpectedly', exc_info=e)
 
-    async def __get_all_messages(self) -> list[MessageEntity]:
+    async def __get_all_alarm_messages(self) -> list[MessageEntity]:
         idx = 1
         all_messages = []
         while True:
@@ -70,6 +72,9 @@ class MessageHandler:
                 if message_list.messages and len(message_list.messages) > 0:
                     all_messages.extend(message_list.messages)
                 else:
+                    return all_messages
+                oldest_message = self.__get_oldest_message(all_messages)
+                if oldest_message is not None and oldest_message.message_time < self.last_message_ts:
                     return all_messages
             except Exception as e:
                 LOG.exception(
@@ -80,7 +85,7 @@ class MessageHandler:
             finally:
                 idx = idx + 1
 
-    async def __delete_message(self, latest_vehicle_start_message):
+    async def __delete_message(self, latest_vehicle_start_message: MessageEntity):
         try:
             message_id = latest_vehicle_start_message.messageId
             await self.saicapi.delete_message(message_id=message_id)
@@ -88,7 +93,7 @@ class MessageHandler:
         except Exception as e:
             LOG.exception('Could not delete message from server', exc_info=e)
 
-    async def __read_message(self, message):
+    async def __read_message(self, message: MessageEntity):
         try:
             message_id = message.messageId
             await self.saicapi.read_message(message_id=message_id)
@@ -110,10 +115,19 @@ class MessageHandler:
             return True
 
     @staticmethod
-    def __get_latest_message(vehicle_start_messages):
+    def __get_latest_message(vehicle_start_messages: list[MessageEntity]) -> Union[MessageEntity, None]:
         return next(iter(reversed(
             sorted(
                 vehicle_start_messages,
                 key=lambda m: m.message_time
             )
         )), None)
+
+    @staticmethod
+    def __get_oldest_message(vehicle_start_messages: list[MessageEntity]) -> Union[MessageEntity, None]:
+        return next(iter(
+            sorted(
+                vehicle_start_messages,
+                key=lambda m: m.message_time
+            )
+        ), None)
